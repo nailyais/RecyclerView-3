0253537e3bbb43cefb4251b82ae747e556f86d1e
Naily Ais Hanani <hananinailyais.nah@gmail.com>
2016-11-16 17:45:52 +0700

    RecyclerView Edit

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview3/InputActivity.java
@@ -52,7 +52,24 @@ public class InputActivity extends AppCompatActivity {
 
         });
 
+        hotel = (Hotel) getIntent().getSerializableExtra(MainActivity.HOTEL);
+        if (hotel != null) {
+            setTitle("Edit" + hotel.judul);
+            fillData();
+        } else {
+            setTitle("New Hotel");
+        }
 
+
+    }
+
+    private void fillData() {
+
+        etJudul.setText(hotel.judul);
+        etDeskripsi.setText(hotel.deskripsi);
+        etDetail.setText(hotel.detail);
+        uriFoto = Uri.parse(hotel.foto);
+        ivFoto.setImageURI(uriFoto);
     }
 
     private void pickPhoto() {
